2ea303221ecd16ad4685008fa32de5359b22736e
Jeff <jeff_lim408@icloud.com>
2023-05-20 15:26:29 -0500

    list-products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { sampleProducts } from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <header>
-        Amazion
-      </header>
+      <header>Amazion</header>
       <main>
-        
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img src={product.image} alt={product.name} className='product-image'/>
+              <h2>{product.name}</h2>
+              <p>${product.price}.00</p>
+            </li>
+          ))}
+        </ul>
       </main>
-      <footer>
-        All rights reserved
-      </footer>
+      <footer>All rights reserved</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
